e4d498a753d1d76d6e078e80c5e6753280b12c03
Sean <sean820117@gmail.com>
2022-11-02 15:13:18 +0800

    winio keydown/up receive virtual keycode

--- src/common/winio_key.py
+++ src/common/winio_key.py
@@ -1,6 +1,7 @@
 import pywinio
 import time
 import atexit
+import ctypes
 # Scancodes references : https://www.win.tue.nl/~aeb/linux/kbd/scancodes-1.html
 
 # KeyBoard Commands
@@ -9,7 +10,10 @@ KBC_KEY_CMD = 0x64
 # Data port
 KBC_KEY_DATA = 0x60
 
+MAPVK_VK_TO_VSC = 0
+
 g_winio = None
+user32 = ctypes.WinDLL('user32', use_last_error=True)
 
 def get_winio():
     global g_winio
@@ -34,20 +38,22 @@ def wait_for_buffer_empty():
     while (dwRegVal & 0x02):
             dwRegVal = winio.get_port_byte(KBC_KEY_CMD)
 
-def key_down(scancode):
+def key_down(virtual_code):
     winio = get_winio()
 
     wait_for_buffer_empty();
     winio.set_port_byte(KBC_KEY_CMD, 0xd2);
     wait_for_buffer_empty();
+    scancode = user32.MapVirtualKeyExW(virtual_code, MAPVK_VK_TO_VSC, 0)
     winio.set_port_byte(KBC_KEY_DATA, scancode)
 
-def key_up(scancode):
+def key_up(virtual_code):
     winio = get_winio()
 
     wait_for_buffer_empty();
     winio.set_port_byte( KBC_KEY_CMD, 0xd2);
     wait_for_buffer_empty();
+    scancode = user32.MapVirtualKeyExW(virtual_code, MAPVK_VK_TO_VSC, 0)
     winio.set_port_byte( KBC_KEY_DATA, scancode | 0x80);
 
 def key_press(scancode, press_time = 0.2):
